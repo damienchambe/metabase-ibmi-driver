6d18bd4e425e784c266d358262948cef5665d0a2
Damien Chambe <42462209+damienchambe@users.noreply.github.com>
2022-04-20 00:09:47 +0200

    update to 0.42 with new metabase build system

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
-(defproject metabase/db2-driver "1.0.38"
+(defproject metabase/db2fori-driver "1.0.42"
   :min-lein-version "2.5.0"
 
   :profiles
   {:provided
    {:dependencies [
      [metabase-core "1.0.0-SNAPSHOT"]
-     [net.sf.jt400/jt400 "8.5"]
+     [net.sf.jt400/jt400 "10.7"]
    ]}
+
+   :clean-targets [:target-path "build/js/output"]
 
    :uberjar
    {:auto-clean    true
     :aot :all
     :javac-options ["-target" "1.8", "-source" "1.8"]
     :target-path   "target/%s"
-    :uberjar-name  "db2.metabase-driver.jar"}})
+    :uberjar-name  "db2fori.jar"}})
